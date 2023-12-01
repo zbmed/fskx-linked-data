add7992d9c61347787e8831ae14ba0622690226b
Julian Schneider <schneider@zbmed.de>
2023-12-01 15:54:27 +0100

    Allow choosing output format
    possible options:
    - JSON-LD (default)
    - N-Quads

--- kg-generator.py
+++ kg-generator.py
@@ -5,7 +5,10 @@ from argparse import ArgumentParser
 
 argparser = ArgumentParser()
 argparser.add_argument('--modelfile', '-M', type=str, help='Relative path to a model\'s filled-out metadata JSON file')
+argparser.add_argument('--format_output', '-F', default='jsonld', type=str, help='Output format: "jsonld" (default) or "nquads"')
 args = argparser.parse_args()
+
+assert args.format_output in ['jsonld', 'nquads'], 'The supplied format ('+args.format_output+') should be "jsonld" (default) or "nquads"!'
 
 with open(args.modelfile) as file:
     modeljson = file.read()
@@ -17,9 +20,14 @@ merge = dict(context, **model)
 
 expanded = jsonld.expand(merge)
 
-normalized = jsonld.normalize(expanded, {'algorithm': 'URDNA2015', 'format': 'application/n-quads'})
-
-with open('graph-out.nq', 'w') as outfile:
-    outfile.write(normalized)
+if args.format_output == 'nquads':
+    normalized = jsonld.normalize(expanded, {'algorithm': 'URDNA2015', 'format': 'application/n-quads'})
+    with open('graph-out.nq', 'w') as outfile:
+        outfile.write(normalized)
+        print('Wrote output to: "graph-out.nq"')
+else: #default: args.output_format == 'jsonld'
+    with open('graph-out.jsonld', 'w') as outfile:
+        outfile.write(json.dumps(expanded))
+        print('Wrote output to: "graph-out.jsonld"')
 
 print('fin')
